0c19dc95366c587fe1763336fdc4847486bb5579
David Yartey <yarteyd@gmail.com>
2019-03-05 05:26:07 +0000

    updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as moment from 'moment';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  experienceDate: string;
+  projects: any;
   constructor() { }
 
   ngOnInit() {
+    this.experienceDate = moment('20170801', 'YYYYMMDD').fromNow();
+    this.getPersonalProjects();
+  }
+
+  getPersonalProjects() {
+    this.projects = [
+      {
+        'name': 'Handy Node',
+        'repo': '', 'description': 'A mini node js framework with a laravel feel. Its main focus',
+        'image': ''
+      },
+      {
+        'name': 'Handy Node',
+        'repo': '', 'description': 'A mini node js framework with a laravel feel. Its main focus',
+        'image': ''
+
+      },
+    ];
   }
 
 }
