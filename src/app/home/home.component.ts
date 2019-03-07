2b7c9a71c23bdbc54bf81a913fe86068a5f52ab8
David Yartey <yarteyd@gmail.com>
2019-03-07 06:05:36 +0000

    added properties and classes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,28 +7,65 @@ import * as moment from 'moment';
 })
 export class HomeComponent implements OnInit {
 
+  email: string;
+  phone: string;
   experienceDate: string;
   projects: any;
+  skills: any;
   constructor() { }
 
   ngOnInit() {
     this.experienceDate = moment('20170801', 'YYYYMMDD').fromNow();
+    this.getSkills();
     this.getPersonalProjects();
+    this.getContactInfo();
+  }
+  getContactInfo() {
+    this.email = 'yarteyd@gmail.com';
+    this.phone = '233 54 994 0617';
   }
 
   getPersonalProjects() {
     this.projects = [
       {
         'name': 'Handy Node',
-        'repo': '', 'description': 'A mini node js framework with a laravel feel. Its main focus',
+        'repo': '',
+        'description': 'A mini node js framework with a laravel feel. Its main focus',
         'image': ''
       },
       {
-        'name': 'Handy Node',
-        'repo': '', 'description': 'A mini node js framework with a laravel feel. Its main focus',
+        'name': 'Mean Auth',
+        'repo': '',
+        'description': 'A mini node js framework with a laravel feel. Its main focus',
         'image': ''
 
       },
+    ];
+  }
+
+  getSkills() {
+    this.skills = [
+      {
+        'name' : 'laravel'
+      },
+      {
+        'name' : 'lumen'
+      },
+      {
+        'name' : 'express js'
+      },
+      {
+        'name' : 'rabbit mq'
+      },
+      {
+        'name' : 'node js'
+      },
+      {
+        'name' : 'api development'
+      },
+      {
+        'name' : 'angular'
+      }
     ];
   }
 
